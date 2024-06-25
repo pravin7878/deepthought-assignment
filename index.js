e0cfeafaadba8f7ef8c944ec7aebb46d75bdc424
pravin7878 <143276463+pravin7878@users.noreply.github.com>
2024-06-25 14:18:31 +0530

    update

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const taskCount = document.querySelector(".card-count>h3")
 const taskInfo = document.querySelector(".tast-info")
 const taskContener = document.querySelector(".main")
 
-const heading = document.querySelector(".main>h1")
+const heading = document.querySelector(".main>div>h1")
 
 
 
@@ -135,7 +135,7 @@ const createTaskCard = (asset) => {
     } = asset
 
     let card = document.createElement("div")
-
+    // coman Element in card
     let head = document.createElement("div")
     head.className = "head-titel"
 
@@ -146,16 +146,13 @@ const createTaskCard = (asset) => {
     let img = document.createElement("img")
     img.src = "./assets/Group 1735.png"
     imgCont.append(img)
-    
-head.append(h3,imgCont)
+
+    head.append(h3, imgCont)
 
     let p = document.createElement("p")
     p.innerText = `Description : ${asset_description}`
 
-
-
-
-
+    // diffrent element in the card basd on the content type
     if (asset_content_type === "videoCont") {
         let videoCont = document.createElement("iframe")
         videoCont.src = asset_content
@@ -163,7 +160,77 @@ head.append(h3,imgCont)
     }
 
     else if (asset_content_type == "threadbuilder") {
-        card.append(head, p)
+        let threadCont = document.createElement("div")
+        threadCont.className = "thread-cont"
+
+        let threadBtn = document.createElement("button")
+        threadBtn.innerHTML = `<span><img src="./assets/upAroow.png"/>Thread A</span> 
+`
+        let threadChild = document.createElement("div")
+        threadChild.className = "thread-Child"
+        
+        // hare is collepsed div
+        const collepsedDiv = document.createElement("div")
+        collepsedDiv.className = "colleps-div"
+        collepsedDiv.innerHTML = `
+        <div>
+               <h3>Sub thead 1</h3>
+               <textarea placeholder="Enter Text here" ></textarea>
+           </div>
+           <div>
+               <h3>Sub Interpretion 1</h3>
+               <textarea placeholder="Enter Text here"></textarea>
+           </div>
+          `
+        // hare is constent div
+        const constentdDiv = document.createElement("div")
+        constentdDiv.className = "const-div"
+        constentdDiv.innerHTML = `<div className = "const-child1">
+        <div>
+            <img src="./assets/blab.png" alt="blab">
+            <img src="./assets/msg.png" alt="msg">
+            <img src="./assets/qna.png" alt="qna">
+            <img src="./assets/icon4.png" alt="feadback">
+        </div>
+
+        <button>
+            <select>
+                <option>Select Categ</option>
+            </select>
+        </button>
+
+        <button>
+            <select>
+                <option>Select Proces</option>
+            </select>
+        </button>
+    </div>
+<button>+ Sub thread</button>
+
+<div>
+    <h3>Summary for Thread A</h3>
+    <textarea placeholder="Enter Text Here"></textarea>
+</div>
+`
+        // hare is colleps logic
+        threadBtn.addEventListener('click', () => {
+            if (collepsedDiv.style.display === "none") {
+                collepsedDiv.style.display = "flex"
+
+                threadBtn.innerHTML = `<span><img src="./assets/dounAroow.png"/>Thread A</span> 
+ `
+            }
+            else {
+                collepsedDiv.style.display = "none"
+                threadBtn.innerHTML = `<span><img src="./assets/upAroow.png"/>Thread A</span> 
+`
+            }
+        })
+
+        // hare is i append all content
+        threadChild.append(collepsedDiv, constentdDiv)
+        threadCont.append(threadBtn, threadChild)
+        card.append(head, p, threadCont)
     }
 
     else if (asset_content_type == "article" && asset_type == "input_asset") {
